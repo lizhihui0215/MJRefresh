e0fa05e2569444794071507986c87ef86c6268eb
lizhihui0215 <lizhihui0215@gmail.com>
2017-01-06 18:49:51 +0800

    fix bugs

--- MJRefresh/MJRefresh.h
+++ MJRefresh/MJRefresh.h
@@ -7,6 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIScrollView+MJRefresh.h"
+#import "UIScrollView+MJExtension.h"
+#import "UIView+MJExtension.h"
+#import "MJRefreshNormalHeader.h"
+#import "MJRefreshGifHeader.h"
+#import "MJRefreshBackNormalFooter.h"
+#import "MJRefreshBackGifFooter.h"
+#import "MJRefreshAutoNormalFooter.h"
+#import "MJRefreshAutoGifFooter.h"
 
 //! Project version number for MJRefresh.
 FOUNDATION_EXPORT double MJRefreshVersionNumber;
