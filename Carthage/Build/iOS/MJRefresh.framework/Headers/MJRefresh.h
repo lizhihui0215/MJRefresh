d49ea7433cbb7983f6903ec6caccd7e83d100a06
lizhihui0215 <lizhihui0215@gmail.com>
2017-01-06 17:42:26 +0800

    add carthage support

---
+++ Carthage/Build/iOS/MJRefresh.framework/Headers/MJRefresh.h
@@ -0,0 +1,19 @@
+//
+//  MJRefresh.h
+//  MJRefresh
+//
+//  Created by 李智慧 on 06/01/2017.
+//  Copyright © 2017 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJRefresh.
+FOUNDATION_EXPORT double MJRefreshVersionNumber;
+
+//! Project version string for MJRefresh.
+FOUNDATION_EXPORT const unsigned char MJRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJRefresh/PublicHeader.h>
+
+
